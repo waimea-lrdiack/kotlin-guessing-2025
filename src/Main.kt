e8e101d7fbff1baa1aaae5a2e034f531f336ac9a
waimea-lrdiack <lrdiack@waimea.school.nz>
2025-02-17 11:00:34 +1300

    using functions

--- src/Main.kt
+++ src/Main.kt
@@ -9,40 +9,48 @@ fun main() {
     println("The game that involves no skill.")
     println()
 
+    var playerName = getString("Enter your name:")
+    println("Nice to meet you $playerName")
+
+
     val numToGuess = (1..100).random()
 
     println("I'm thinking of a number from 1 to 100.")
     println("Can you guess it?")
+    println()
+
     var guessCount = 0
 
     // main loop\
 
     while (true) {
-        // Ask the user for their first guess
-        print("Enter your first number:")
-        val input = readLine()?.toIntOrNull()
+        // Ask the user for a guess
+        print("Enter your guess: ")
+        val guess = readLine()?.toIntOrNull()
 
         // check if user is correct or not
-        if (input == null) {
+        if (guess == null) {
             println("Please enter a valid number!")
+            continue
         }
+
         //when correct break out of loop
-        if (input == numToGuess) {
+        if (guess == numToGuess) {
             println("YOU ARE CORRECT!!! :)")
-            println("Thank you for playing!")
+            println("Thank you for playing $playerName!")
             println("You took ${guessCount} guesses to guess it!")
             break
         }
         // otherwise give higher/lower feedback
-        if (input != null) {
-            if (input > 100) {
+        if (guess != null) {
+            if (guess > 100) {
                 println("Choose a lower number please")
             }
 
-            if (input < numToGuess) {
+            if (guess < numToGuess) {
                 println("Higher")
                 guessCount++
-            } else if (input > numToGuess) {
+            } else if (guess > numToGuess) {
                 println("Lower")
                 guessCount++
             }
@@ -52,10 +60,45 @@ fun main() {
         if (guessCount == 10) {
                 println("You are out of guesses")
                 println("The correct number was ${numToGuess}")
-                println("Thank you for playing!")
+                println("Thank you for playing$playerName!")
                 break
             }
         }
 
     }
 
+/**
+ * Function to get a string from the user
+ * parameters:
+ * - prompt - text to show the user
+ * returns:
+ * - String that the user types
+ */
+fun getString(prompt: String): String {
+    var userInput: String
+
+    while(true) {
+        print(prompt)
+
+        userInput = readln()
+        if (userInput.isNotBlank()) break
+    }
+
+    return userInput
+}
+
+/**
+ * Function to get an Int from the user
+ */
+
+fun getInt(prompt: String): Int {
+    var intValue: Int?
+
+    while(true) {
+        val userInput = getString(prompt)
+        intValue = userInput.toIntOrNull()
+        if (intValue != null) break
+    }
+
+    return intValue!!
+}
